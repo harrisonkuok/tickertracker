7631ff1216078463da74cb5cfdea7cd76fdb2122
harrisonkuok <harrisonkuok@gmail.com>
2022-01-06 23:21:15 -0800

    add model

--- tickertracker/tickertrackerapp/models.py
+++ tickertracker/tickertrackerapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10)
+    name = models.CharField(max_length=256)
+    mention = models.IntegerField(default=0, validators=[MaxValueValidator(9999)])
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+    def get_top_mentioned(number):
+        return Stock.objects.order_by('-mention')[:number]
+
+    def __str__(self):
+        return self.ticker
